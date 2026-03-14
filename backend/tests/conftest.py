339e1aacd1d766769dd68ed05de1a8542ec39071
らぐな <pasupoto17@gmail.com>
2026-03-14 14:50:18 +0900

    Initial commit

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -21,17 +21,6 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 # -----------------------------
-# Event loop
-# -----------------------------
-
-@pytest.fixture(scope="session")
-def event_loop():
-    loop = asyncio.new_event_loop()
-    yield loop
-    loop.close()
-
-
-# -----------------------------
 # Engine
 # -----------------------------
 
