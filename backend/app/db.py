19c74f940c3b09beb33a93b46a0fe419226715bc
らぐな <pasupoto17@gmail.com>
2026-03-14 15:21:13 +0900

    Initial commit

--- backend/app/db.py
+++ backend/app/db.py
@@ -4,16 +4,37 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_async_engine(DATABASE_URL, echo=False)
-
-async_session = async_sessionmaker(
-    engine,
-    expire_on_commit=False,
-    class_=AsyncSession
-)
+engine = None
+async_session = None
 
 Base = declarative_base()
 
+
+def get_engine():
+    global engine
+
+    if engine is None:
+        engine = create_async_engine(DATABASE_URL, echo=False)
+
+    return engine
+
+
+def get_sessionmaker():
+    global async_session
+
+    if async_session is None:
+        async_session = async_sessionmaker(
+            get_engine(),
+            expire_on_commit=False,
+            class_=AsyncSession,
+        )
+
+    return async_session
+
+
 async def get_db():
-    async with async_session() as session:
+
+    sessionmaker = get_sessionmaker()
+
+    async with sessionmaker() as session:
         yield session
